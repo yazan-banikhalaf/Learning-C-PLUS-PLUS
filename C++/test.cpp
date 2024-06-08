820fb84a0a74b279b8300c65f5a31594206a09e9
Yazan Banikhalaf <103342472+yazan-banikhalaf@users.noreply.github.com>
2024-06-08 16:23:24 +0300

    Update test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,18 +1,29 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 int main()
 {
-	string name;
-	int age;
-	cout << "Enter Your Age: ";
-	cin >> age;
-	cout << "Your Age Is: " << age;
+	int temp;
+	char unit;
 	
-	cout << "Enter Your Name: ";
-	getline(cin >> ws, name);
-	cout << "Hello " << name << endl;
+	cout << "What unit would you like to converte ? (F/C)" << endl;
+	cin >> unit;
 	
-	
+	if (unit == 'f' || unit == 'F'){
+		cout << "Enter the Temprature in Celsius: " << endl;
+		cin >> temp;
+		temp = (1.8 * temp) + 32.0;
+		 
+		cout << "The Temprature is: " << temp << "F" << endl;
+	} else if (unit == 'C' || unit == 'c'){
+		cout << "Enter the Temprature in Fahrenheit: " << "C" << endl;
+		cin >> temp;
+		temp = (temp - 32) / 1.8;
+		 
+		cout << "The Temprature is: " << temp << endl;
+	} else {
+		cout << "Please Enter only C or F" << endl;
+	}
  	return 0;
 }

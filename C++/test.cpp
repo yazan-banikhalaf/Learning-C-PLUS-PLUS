9a9672e48c776766466403e80b480e7e25cdb767
Yazan Banikhalaf <103342472+yazan-banikhalaf@users.noreply.github.com>
2024-06-08 17:08:10 +0300

    Update test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,18 +1,55 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 int main()
 {
 	string name;
-	int age;
-	cout << "Enter Your Age: ";
-	cin >> age;
-	cout << "Your Age Is: " << age;
 	
-	cout << "Enter Your Name: ";
-	getline(cin >> ws, name);
-	cout << "Hello " << name << endl;
+	cout << "Enter your name: ";
+	getline(cin , name);
 	
+	/*******************************************LENGTH METHOD***************************************************/
+	/*
+	if (name.length() > 12){
+		cout << "Your name must be less than 12 charctares" << endl;
+		cout << "The length is " << name.length();
+	} else if(name.empty()) {
+		cout << "Name can not be empty" << endl;
+	} else {
+		cout << "Hello " << name << "\n";
+	}
+	*/
 	
- 	return 0;
+	/*****************************************APPEND METHOD*****************************************************/
+	/*
+	name.append("@gmail.com");
+	
+	cout << "Your Email Is : " << name << endl;
+	*/	
+	
+	/*******************************************AT METHOD***************************************************/
+	/*
+	name = name.at(0);
+	cout << "The First Char is: " << name << endl;
+	*/
+ 	
+	/*******************************************INSERT METHOD***************************************************/
+	/*
+	name.insert(0,"@");
+	cout << name << endl;
+	*/
+	
+	/*******************************************FIND METHOD***************************************************/
+	/*
+	int pos = name.find('a');
+	cout << pos << endl;
+	*/
+	/*******************************************ERASE METHOD***************************************************/
+	/*
+	name.erase(0,3);
+	cout << name << endl;
+	*/
+	
+	return 0;
 }

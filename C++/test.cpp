e869fecca190a640dec42c71a87973e73f70303b
Yazan Banikhalaf <103342472+yazan-banikhalaf@users.noreply.github.com>
2024-06-08 23:27:44 +0300

    Update test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,18 +1,39 @@
 #include<iostream>
+#include <ctime>
+
 using namespace std;
 
 int main()
 {
-	string name;
-	int age;
-	cout << "Enter Your Age: ";
-	cin >> age;
-	cout << "Your Age Is: " << age;
+	srand(time(NULL));
+	int usernumber;
+	int randomNumber;
+	bool won = false;
+	int close;
 	
-	cout << "Enter Your Name: ";
-	getline(cin >> ws, name);
-	cout << "Hello " << name << endl;
+	for(int i = 1; i<=3; i++){
+		randomNumber = (rand() % 6) + 1;
+		cout << "Enter Your Lucky Number!: ";
+		cin >> usernumber;
+		close = abs(usernumber - randomNumber);
+		
+		if(randomNumber == usernumber){
+			cout << "You WON! " << endl; 
+			won = true;
+			break;
+		} else {
+			cout << "You Lost " << endl;
+			cout << "The Lucky Number Is: " << randomNumber << endl;
+		}
+		if(close == 1) {
+			cout << "You won extra try, YEEEEEY " << endl;
+			i--;
+		}
+	}
+	if(!won){
+		cout << "Better luck next time!" << endl;
+		
+	}
 	
-	
- 	return 0;
+	return 0;
 }
